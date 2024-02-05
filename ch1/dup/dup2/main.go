acccd649e23aad08d35bb753591cf88ca0ea51aa
tty <yylykym3@outlook.com>
2024-02-05 17:58:13 +0800

    文件统计相同行数

---
+++ ch1/dup/dup2/main.go
@@ -0,0 +1,35 @@
+// Dup2 prints the count and text of lines that appear more than once
+// in the input.  It reads from stdin or from a list of named files.
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
+
+func main() {
+	counts := make(map[string]int)
+	files := os.Args[1:]
+	if len(files) == 0 {
+		countLines(os.Stdin, counts)
+	}
+	for _, arg := range files {
+		open, err := os.Open(arg)
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "dup2: %v\\n", err)
+		}
+		countLines(open, counts)
+		open.Close()
+	}
+	for line, n := range counts {
+		fmt.Printf("%d\t%s\n", n, line)
+	}
+}
+
+func countLines(f *os.File, counts map[string]int) {
+	input := bufio.NewScanner(f)
+	for input.Scan() {
+		counts[input.Text()]++
+	}
+}
