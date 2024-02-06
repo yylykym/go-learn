89a5c392f4e5960e9143496ac1bb6e28b54187a2
tty <yylykym3@outlook.com>
2024-02-06 17:34:38 +0800

    1

--- ch1/dup/dup3/main.go
+++ ch1/dup/dup3/main.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"fmt"
-	"io/ioutil"
 	"os"
 	"strings"
 )
@@ -10,7 +9,7 @@ import (
 func main() {
 	counts := make(map[string]int)
 	for _, filename := range os.Args[1:] {
-		data, err := ioutil.ReadFile(filename)
+		data, err := os.ReadFile(filename)
 		if err != nil {
 			fmt.Fprintf(os.Stderr, "dup3: %v\n", err)
 			continue
